b85cde987ae8cb8ff1812f8b9d9f8900a1ed3775
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 22:03:03 +0200

    todo funciona correctamente

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-import time
-import random
-import tkinter as tk
-from math import sin, cos, pi
 from cenafil import *
-from filosofo import*
-from tenedor import*
+
+
 
 def iniciar():
     cena = CenaFilosofos()
