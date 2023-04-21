6cd45466f24ca8d717b0ac89841f74f4d649ae63
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 20:45:27 +0200

    son pruebas que borramos

--- filosofos.py
+++ filosofos.py
@@ -3,12 +3,6 @@ import time
 import random
 import tkinter as tk
 
-# Constantes para colores
-BLANCO = "#FFFFFF"
-NEGRO = "#000000"
-GRIS = "#D3D3D3"
-ROJO = "#FF0000"
-VERDE = "#008000"
 
 # Constantes para posiciones de los filósofos y los tenedores
 POSICIONES_FILOSOFOS = [(300, 100), (450, 250), (450, 450), (300, 600), (100, 450), (100, 250)]
@@ -29,6 +23,11 @@ class Tenedor:
         self.tenedor.release()
         print("Filósofo", filosofo, "liberó tenedor", tenedor)
 
+    def color(self):
+        if self.tenedor.locked(): #si el tenedor esta bloqueado
+            return 'blue'
+        return 'grey'
+
 class Filosofo(threading.Thread):
     def __init__(self, id, tenedor_izq, tenedor_der, cena):
         threading.Thread.__init__(self)
@@ -41,17 +40,17 @@ class Filosofo(threading.Thread):
     def run(self):
         while True:
             time.sleep(random.randint(1, 5))
-            self.cena.actualizar_filosofo(self.id, "Esperando", GRIS)
+            self.cena.actualizar_filosofo(self.id, "Hambriento", 'pink')
             tenedor_izq_tomado = self.tenedor_izq.tomar(self.id, "izquierdo")
             tenedor_der_tomado = self.tenedor_der.tomar(self.id, "derecho")
             if tenedor_izq_tomado and tenedor_der_tomado:
-                self.cena.actualizar_filosofo(self.id, "Comiendo", VERDE)
+                self.cena.actualizar_filosofo(self.id, "Comiendo", 'yellow')
                 self.comidas += 1
                 self.cena.actualizar_contador(self.id, self.comidas)
                 time.sleep(random.randint(1, 5))
                 self.tenedor_der.liberar(self.id, "derecho")
                 self.tenedor_izq.liberar(self.id, "izquierdo")
-                self.cena.actualizar_filosofo(self.id, "Pensando", ROJO)
+                self.cena.actualizar_filosofo(self.id, "Pensando", 'white')
             else:
                 if tenedor_izq_tomado:
                     self.tenedor_izq.liberar(self.id, "izquierdo")
@@ -61,7 +60,8 @@ class Filosofo(threading.Thread):
 class CenaFilosofos:
     def __init__(self):
         self.ventana = tk.Tk()
-        self.canvas = tk.Canvas(self.ventana, width=600, height=700, bg=BLANCO)
+        self.ventana.title("Cena de los Filósofos")
+        self.canvas = tk.Canvas(self.ventana, width=600, height=700, bg='white')
         self.canvas.pack()
         self.filosofos = []
         self.tenedores = []
@@ -72,25 +72,26 @@ class CenaFilosofos:
             self.filosofos.append(filosofo)
             self.tenedores.append(tenedor_izq)
             self.tenedores.append(tenedor_der)
-            self.dibujar_filosofo(i, "Pensando", ROJO)
+            self.dibujar_filosofo(i, "Pensando", 'white')
             self.dibujar_tenedor(i)
         self.contadores = []
-        texto_explicativo= 'Rojo: Hambriento\nAmarillo: Comiendo\nBlanco: Pensando'
+        texto_explicativo= 'Rosa: Hambriento\nAmarillo: Comiendo\nBlanco: Pensando'
         texto_explicativo= tk.Label(self.ventana, text=texto_explicativo, bg='white')
         texto_explicativo.pack()
         for i in range(6):
-            contador = tk.Label(self.ventana, text="Filósofo " + str(i) + ": 0", bg=BLANCO)
+            contador = tk.Label(self.ventana, text="Filósofo " + str(i) + ": 0", bg='white')
             contador.place(x=520, y=100 + i * 75)
             self.contadores.append(contador)
 
     def dibujar_filosofo(self, id, estado, color):
         x, y = POSICIONES_FILOSOFOS[id]
-        self.canvas.create_oval(x-25, y-25, x+25, y+25, fill=color, outline=NEGRO)
+        self.canvas.create_oval(x-25, y-25, x+25, y+25, fill=color, outline='black')
         self.canvas.create_text(x, y, text="Filósofo " + str(id) + "\n" + estado)
 
     def dibujar_tenedor(self, id):
         x, y = POSICIONES_TENEDORES[id]
-        self.canvas.create_rectangle(x-10, y-10, x+10, y+10, fill=NEGRO)
+        color = self.tenedores[id].color()
+        self.canvas.create_rectangle(x-10, y-10, x+10, y+10, fill=color)
 
     def actualizar_filosofo(self, id, estado, color):
         self.canvas.delete("filosofo"+str(id))
@@ -109,4 +110,4 @@ if __name__ == "__main__":
     cena.iniciar_cena()
 
 
-#TODO: MEJORAR CODIGO
+
