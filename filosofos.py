e528bea3855485213af6ebb6c773fea38e6fac3a
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 11:54:15 +0200

    definmos el constructor de la clase filosofo

--- filosofos.py
+++ filosofos.py
@@ -3,3 +3,13 @@
 import threading
 import time
 import tkinter as tk
+
+class Filosofo():
+    def __init__(self, nombre, tenedorIzq, tenedorDer, ventana):
+        self.nombre = nombre
+        self.tenedorIzq = tenedorIzq
+        self.tenedorDer = tenedorDer
+        self.ventana = ventana
+        self.estado = "PENSANDO"
+        self.numComidas = 0
+
