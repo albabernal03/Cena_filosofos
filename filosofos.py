91d4f3a343102206780e54b90833164c9e8fdb6e
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 11:58:05 +0200

    Terminamos de definir la función coger tenedor

--- filosofos.py
+++ filosofos.py
@@ -12,4 +12,17 @@ class Filosofo():
         self.ventana = ventana
         self.estado = "PENSANDO"
         self.numComidas = 0
+        self.actualizar_ventana()
 
+    def coger_tenedores(self):
+        tenedorIzq, tenedorDer = self.tenedorIzq, self.tenedorDer
+        while True:
+            tenedorIzq.acquire() #Adquiere el tenedor izquierdo
+            adq_tenedorIzq = tenedorIzq.locked() #Comprueba si el tenedor izquierdo esta bloqueado
+            tenedorDer.acquire() #Adquiere el tenedor derecho
+            adq_tenedorDer = tenedorDer.locked() #Comprueba si el tenedor derecho esta bloqueado
+            if adq_tenedorIzq and adq_tenedorDer: 
+                self.estado="COMIENDO"
+                self.numComidas += 1
+                self.actualizar_ventana()
+                break
