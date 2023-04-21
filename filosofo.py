716a4ef7d6ad24611c568be6fddbe7ab3ae4e2e1
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 21:53:20 +0200

    ordenando

---
+++ filosofo.py
@@ -0,0 +1,33 @@
+import threading
+import time
+import random
+from tenedor import *
+
+class Filosofo(threading.Thread):
+    def __init__(self, id, tenedor_izq, tenedor_der, cena):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.tenedor_izq = tenedor_izq
+        self.tenedor_der = tenedor_der
+        self.cena = cena
+        self.comidas = 0
+
+    def run(self):
+        while True:
+            time.sleep(random.randint(1, 5))
+            self.cena.actualizar_filosofo(self.id, "Hambriento", 'pink')
+            tenedor_izq_tomado = self.tenedor_izq.tomar(self.id, "izquierdo")
+            tenedor_der_tomado = self.tenedor_der.tomar(self.id, "derecho")
+            if tenedor_izq_tomado and tenedor_der_tomado:
+                self.cena.actualizar_filosofo(self.id, "Comiendo", 'yellow')
+                self.comidas += 1
+                self.cena.actualizar_contador(self.id, self.comidas)
+                time.sleep(random.randint(1, 5))
+                self.tenedor_der.liberar(self.id, "derecho")
+                self.tenedor_izq.liberar(self.id, "izquierdo")
+                self.cena.actualizar_filosofo(self.id, "Pensando", 'light blue')
+            else:
+                if tenedor_izq_tomado:
+                    self.tenedor_izq.liberar(self.id, "izquierdo")
+                if tenedor_der_tomado:
+                    self.tenedor_der.liberar(self.id, "derecho")
---
+++ tenedor.py
@@ -0,0 +1,28 @@
+import threading
+import time
+import random
+import tkinter as tk
+
+class Tenedor:
+    def __init__(self, id):
+        self.id = id #esto nos 
+        self.tenedor = threading.Lock()
+        self.en_uso= False
+
+    def tomar(self, filosofo, tenedor):
+        if self.tenedor.acquire(blocking=False):
+            print("Filósofo", filosofo, "tomó tenedor", tenedor)
+            self.en_uso= True
+            return True
+        return False
+
+    def liberar(self, filosofo, tenedor):
+        self.tenedor.release()
+        self.en_uso= False
+        print("Filósofo", filosofo, "liberó tenedor", tenedor)
+        
+
+    def color(self):
+        if self.en_uso:
+            return 'blue'
+        return 'grey'
