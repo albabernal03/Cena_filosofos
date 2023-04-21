de226eecdcfb11e866d32831ec7464f3d9edf164
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 12:25:14 +0200

    definimos la clase filosofos

--- filosofos.py
+++ filosofos.py
@@ -9,3 +9,12 @@ posiciones = [(200, 50), (300, 150), (300, 250), (200, 350), (100, 250), (100, 1
 #Definimos la posición de los tenedores
 posiciones_tenedores = [(250, 100), (350, 200), (350, 300), (250, 400), (150, 300), (150, 200)]
 
+#Definimos la clase filosofo
+class Filosofo(threading.Thread): #la clase filosofo es un hilo
+    def __init__(self,nombre,tenedorIzq,tenedorDer,actualizar_ventana):
+        threading.Thread.__init__(self)
+        self.nombre = nombre
+        self.tenedorIzq = tenedorIzq
+        self.tenedorDer = tenedorDer
+        self.actualizar_ventana = actualizar_ventana
+        self.contador = 0 #contador de veces que come el filosofo
